f2c6b2af08f8b7b8555d35f7ea3e31d36f827f60
iadam <iadam@ADAM>
2024-01-02 20:37:49 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GA_WriteLine_Adam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" Jake");
+            Console.WriteLine("Adam");
+        } 
+          
+    }
+}
